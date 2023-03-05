3130ef6f354070b5dbec03708db942a4c29800c0
subkt2013 <49891514+subkt2013@users.noreply.github.com>
2023-03-05 22:56:39 +0900

    TailwindCSSの導入

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+  <title>予約システム</title>
+</head>
+<body>
+  @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('web.index');
+});
+
+Route::get('/welcome', function () {
     return view('welcome');
 });
